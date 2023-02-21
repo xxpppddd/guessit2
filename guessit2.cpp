a0b7c4dcbdd6d87d64a69b0051f544cabc71876d
xxpppddd <118286404+xxpppddd@users.noreply.github.com>
2023-02-21 14:44:10 +0700

    Add files via upload

---
+++ guessit2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int selectNumber (int low, int high) {
+    return rand() % (high - low+1) + low;
+}
+char getHostAnswer(int X) {
+    char answer;
+    cout<< " Is " << X << " your number? \n";
+    cin>>answer;
+    return answer;
+}
+int main() {
+    int X;
+    char answer;
+    int low = 1, high = 100;
+    while (answer != '=') {
+        X = selectNumber (low, high);
+        answer = getHostAnswer(X);
+        if (answer == '=') {
+            cout << "Your number is " <<X<<"\n";
+        }
+        else if (answer == '>') {
+            high = X-1;
+        }
+        else {
+            low = X+1;
+        }
+    }
+
+}
